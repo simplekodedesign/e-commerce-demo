f653d3a62bcdab8e3af94bf3997f03f87cca7acd
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-01-02 15:18:28 +0100

    Express Server

---
+++ src/database.js
@@ -0,0 +1,13 @@
+//modulo para utilizar bases de datos Mongo
+const mongoose = require("mongoose")
+
+//utilizar las variables de entorno
+require("dotenv").config()
+
+//establecer conexion a la base de datos
+mongoose.connect(process.env.URL_DATABASE,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(db => console.log("DB Connected"))
+	.catch(err => console.log("Something Wrong"))
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express") //servidor
+const morgan = require("morgan") //ver peticiones en terminal
+const path = require("path")
+require("./database") //conexion a la base de datos
+
+//inicializar el servidor
+const app = express()
+
+//utilizar las variables de entorno
+require("dotenv").config()
+
+//configuraciones
+app.set("port",process.env.PORT || 3000) //establecer puerto
+
+//middlewares
+app.use(morgan("dev")) //ver las peticiones al servidor por la consola
+app.use(express.json()) //establecer la forma en como se realiza el intercambio de datos
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,"public")))
+
+/*------------ROUTES----------------*/
+
+
+//arrancar el servidor
+app.listen(app.get("port"),() => {
+	console.log("Server on port: " + app.get("port"))
+})
