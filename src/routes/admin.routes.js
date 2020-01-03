d59d0d863ba5b3e5aebeb33990d8ebb1271357f3
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-01-03 17:04:00 +0100

    Currency Routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require("express")
 const router = Router() //establecer router
 
-
+router.use("/currency",require("./currency.routes")) //controlar las monedas en las que se presenta el precio
 
 module.exports = router
--- src/routes/currency.routes.js
+++ src/routes/currency.routes.js
@@ -1,6 +1,62 @@
 const {Router} = require("express")
 const router = Router()
 
+//importar modelos
+const Currency = require("../models/Currency")
 
+//añadir moneda
+router.post("/add",async (req,res) => {
+	const {currency,value} = req.body
+
+	if(!currency || !value)
+		return res.json({
+			status: -1,
+			message: "Debe rellenar todos los campos"
+		})
+
+	const newCurrency = new Currency({
+		currency,
+		value
+	})
+
+	await newCurrency.save()
+
+	res.json(newCurrency)
+})
+
+//ver las monedas existentes
+router.get("/",async (req,res) => {
+	const currency = await Currency.find()
+	res.json(currency)
+})
+
+//modificar moneda
+router.post("/update",async (req,res) => {
+	const {currency_id,currency,value} = req.body
+	
+	if(!currency_id || !currency || !value)
+		return res.json({
+			status: -1,
+			message: "Debe rellenar todos los campos"
+		})
+
+	await Currency.update({_id: currency_id},{
+		currency,
+		value
+	})
+
+	res.json({
+		status: 1,
+		message: "Moneda Actualizada con exito"
+	})
+})
+
+//elimiar moneda
+router.post("/delete",async (req,res) => {
+	const {currency_id} = req.body
+	await Currency.remove({_id: currency_id})
+	const currencies = await Currency.find()
+	res.json(currencies)
+})
 
 module.exports = router
