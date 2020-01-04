080843785c437700d7f7ad593701e2f629a43fd2
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-01-04 04:08:56 +0100

    Product Routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,22 @@
 const {Router} = require("express")
 const router = Router() //establecer router
 
-router.use("/currency",require("./currency.routes")) //controlar las monedas en las que se presenta el precio
+router.use("/currency",ensureAdmin,require("./currency.routes")) //controlar las monedas en las que se presenta el precio
 router.use("/product",require("./product.routes")) //controlar los productos
+
+//validar que el usuario sea administrador
+function ensureAdmin(req,res,next){
+	console.log(req.headers)
+
+	const isAdmin = req.headers["is_admin"]
+	
+	if(!isAdmin)
+		res.json({
+			status: -1,
+			message: "No tienes acceso a esta ruta"
+		})
+
+	next()
+}
 
 module.exports = router
