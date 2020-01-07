a20118128227598239a1c870d2037cf50781c767
Daniel Salazar <dnlsalazar27@gmail.com>
2020-01-07 18:59:49 -0400

    Working on requests

--- src/app/components/Store.js
+++ src/app/components/Store.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Products_Board from './Store_Components/Products_Board'
-import Search_Bar from './Store_Components/Search_Bar'
+import Nav_Bar from './Store_Components/Nav_Bar'
 
 function Store () {
-  const [category, setCategory] = useState("")
+  const [, setCategory] = useState("")
   const [data, setData] = useState({})
 
   useEffect(() => {
@@ -15,8 +15,7 @@ function Store () {
 
   return (
     <div className="store">
-      <Search_Bar setCategory={setCategory} />
-      <h1>Showing { category } category</h1>
+      <Nav_Bar setCategory={setCategory} />
       <Products_Board products={data} />
     </div>
   )
